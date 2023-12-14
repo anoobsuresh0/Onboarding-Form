c31efda9952c6c2004f1957cbe2554a7282a7a38
anoob-perleybrook <anoob.suresh@perleybrook.com>
2023-12-14 14:43:39 +0530

    Install tailwind and starter template

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,20 @@
+// import React from 'react'
+
+import MainPage from "./pages/MainPage";
+import SideBar from "./pages/SideBar";
+
+const HomePage = () => {
+  return (
+    <div className="g">
+      <div>
+        <SideBar />
+      </div>
+
+      <div className="" >
+        <MainPage />
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/components/pages/SideBar.tsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+
+const SideBar = () => {
+  return (
+    <div>
+        <h1>
+            SIDEBAR
+        </h1>
+    </div>
+  )
+}
+
+export default SideBar
